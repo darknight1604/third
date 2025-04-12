c5fa0b1479cc724d422fafb60d8f3ab8905959c5
Huỳnh Quốc Tấn <65750436+darknight1604@users.noreply.github.com>
2025-04-12 14:50:07 +0700

    feat: update dateTimeUtil

--- src/utils/dateTimeUtil.ts
+++ src/utils/dateTimeUtil.ts
@@ -1,28 +1,16 @@
+import dayjs from 'dayjs';
+import {DATE_TIME_FORMAT} from '../constants';
+
 export function formatDate(
   date: Date,
-  pattern: string = 'YYYY-MM-DD HH:mm:ss',
+  pattern: string = DATE_TIME_FORMAT.FORMAT1,
 ): string {
-  const year = date.getFullYear().toString();
-  const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Month is 0-indexed
-  const day = date.getDate().toString().padStart(2, '0');
-  const hours = date.getHours().toString().padStart(2, '0');
-  const minutes = date.getMinutes().toString().padStart(2, '0');
-  const seconds = date.getSeconds().toString().padStart(2, '0');
+  return dayjs(date).format(pattern);
+}
 
-  const replacements: {[key: string]: string} = {
-    YYYY: year,
-    MM: month,
-    DD: day,
-    HH: hours,
-    mm: minutes,
-    ss: seconds,
-  };
-
-  let formatted = pattern;
-
-  for (const [key, value] of Object.entries(replacements)) {
-    formatted = formatted.replace(key, value);
-  }
-
-  return formatted;
+export function parse(
+  value: string,
+  pattern: string = DATE_TIME_FORMAT.FORMAT1,
+): Date {
+  return dayjs(value, pattern).toDate();
 }
