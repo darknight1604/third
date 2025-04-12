2c7c7d3faf5907e3fc6d76a0ee537ebc7f34fdc9
Huỳnh Quốc Tấn <65750436+darknight1604@users.noreply.github.com>
2025-04-12 16:32:49 +0700

    feat: setup fonts

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -2,8 +2,13 @@ import {NavigationContainer} from '@react-navigation/native';
 import {ReactNode, useMemo} from 'react';
 import {useColorScheme} from 'react-native';
 import {darkTheme, lightTheme} from '../../theme';
-import {MD3LightTheme as DefaultTheme, PaperProvider} from 'react-native-paper';
+import {
+  configureFonts,
+  MD3LightTheme as DefaultTheme,
+  PaperProvider,
+} from 'react-native-paper';
 import {navigationRef} from '../../routes/Navigation';
+import {fontConfig} from '../../fonts';
 
 interface IProvidersProps {
   children: ReactNode;
@@ -15,6 +20,7 @@ const Providers = ({children}: IProvidersProps) => {
     () => ({
       ...DefaultTheme,
       colors: colorScheme === 'dark' ? darkTheme.colors : lightTheme.colors,
+      fonts: configureFonts({config: fontConfig}),
     }),
     [colorScheme],
   );
