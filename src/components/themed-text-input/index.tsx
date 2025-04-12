6fbe9019c88dc6cdda8de6be2fb68bebc12a98fe
Huỳnh Quốc Tấn <65750436+darknight1604@users.noreply.github.com>
2025-04-12 15:50:29 +0700

    feat: update style of TextInput

--- src/components/themed-text-input/index.tsx
+++ src/components/themed-text-input/index.tsx
@@ -1,20 +1,48 @@
-import {TextInput, TextInputProps, useTheme} from 'react-native-paper';
+import {View} from 'react-native';
+import {Text, TextInput, TextInputProps, useTheme} from 'react-native-paper';
+import {styles} from './styles';
+import {useMemo} from 'react';
+import {TextStyle} from 'react-native';
+import {StyleProp} from 'react-native';
 
 interface IThemedTextInputProps extends TextInputProps {
-  label: string;
+  isRequired?: boolean;
   value?: string;
+  errorMsg?: string;
+  label: string;
 }
-const ThemedTextInput = ({label, value, ...rest}: IThemedTextInputProps) => {
+const ThemedTextInput = ({
+  label,
+  value,
+  isRequired,
+  error,
+  errorMsg,
+  ...rest
+}: IThemedTextInputProps) => {
   const theme = useTheme();
+  const errorStyle: StyleProp<TextStyle> = useMemo(() => {
+    return {...styles.error, color: theme.colors.error};
+  }, [theme.colors.error]);
 
   return (
-    <TextInput
-      label={label}
-      value={value}
-      mode="outlined"
-      outlineStyle={{borderRadius: theme.roundness}}
-      {...rest}
-    />
+    <View style={styles.container}>
+      <Text>
+        {label}
+        {isRequired && <Text style={errorStyle}>*</Text>}
+      </Text>
+      <TextInput
+        label={undefined}
+        value={value}
+        mode="outlined"
+        outlineColor={error ? theme.colors.error : undefined}
+        activeOutlineColor={error ? theme.colors.error : undefined}
+        outlineStyle={{
+          borderRadius: theme.roundness,
+        }}
+        {...rest}
+      />
+      {error && errorMsg && <Text style={errorStyle}>{errorMsg}</Text>}
+    </View>
   );
 };
 
