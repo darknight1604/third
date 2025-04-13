d952372cd8fe9d83384043c6a74d3fa274a3bf61
Huỳnh Quốc Tấn <65750436+darknight1604@users.noreply.github.com>
2025-04-13 22:24:06 +0700

    refactor: remove example component

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,39 +1,32 @@
-import BottomSheet, {BottomSheetMethods} from '@devvie/bottom-sheet';
-import {useRef} from 'react';
-import {Text, View} from 'react-native';
 import {FloatingAction} from 'react-native-floating-action';
-import {CameraPicker, ThemedTextInput} from '../../components';
 import BaseView from '../../components/base-view';
-import Dropdown from '../../components/dropdown';
 import ThemedText from '../../components/themed-text';
 import {ROUTE_NAME} from '../../constants';
 import {HomeParam} from '../../routes/Navigation';
 
 const Home = ({navigation}: HomeParam) => {
-  const sheetRef = useRef<BottomSheetMethods>(null);
+  // const sheetRef = useRef<BottomSheetMethods>(null);
 
   const onPressFab = () => {
     navigation.navigate(ROUTE_NAME.CREATE_NOTE);
   };
   return (
     <BaseView>
-      <ThemedText>Home Screen 1</ThemedText>
-      <Dropdown
+      <ThemedText>Something awesome is comming</ThemedText>
+      {/* <Dropdown
         label="label"
         value="value"
         onPress={() => sheetRef.current?.open()}
-      />
-      <ThemedTextInput label="hello" inputMode="numeric" />
-      <CameraPicker />
+      /> */}
       <FloatingAction showBackground={false} onPressMain={onPressFab} />
-      <BottomSheet ref={sheetRef}>
+      {/* <BottomSheet ref={sheetRef}>
         <View>
           <Text>
             The smart 😎, tiny 📦, and flexible 🎗 bottom sheet your app craves
             🚀
           </Text>
         </View>
-      </BottomSheet>
+      </BottomSheet> */}
     </BaseView>
   );
 };
